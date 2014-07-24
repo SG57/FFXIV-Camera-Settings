a6d61142b3b524aad6ff14f48a3e7bd94a4bd480
Cord Rehn <jordansg57@gmail.com>
2014-07-24 00:22:56 -0700

    Initial commit.

---
+++ FFXIV Camera Settings/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FFXIV Camera Settings")> 
+<Assembly: AssemblyDescription("A simple program to alter camera settings in FFXIV.")> 
+<Assembly: AssemblyCompany("SG57 Productions")> 
+<Assembly: AssemblyProduct("FFXIV Camera Settings")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ca90bce7-4fa7-4739-8c23-f695adacea2e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ README.md
@@ -0,0 +1,17 @@
+FFXIV Camera Settings
+=========================
+
+A simple program that allows you to change the camera settings in FFXIV.
+<<<<<<< HEAD
+
+
+More details:
+http://www.reddit.com/r/ffxiv/comments/1n8hv6/new_addon_percenthealth_overlay/
+
+
+
+
+Creator:
+  Cord Rehn - <jordansg57@gmail.com>
+
+(C) 2014
